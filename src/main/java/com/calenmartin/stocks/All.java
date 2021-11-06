75c799f287514741c8670677392221bd61ecd0f5
calen <calen@orangeandbronze.com>
2021-11-06 19:46:59 +0800

    Uncommented the first few markets.

--- src/main/java/com/calenmartin/stocks/All.java
+++ src/main/java/com/calenmartin/stocks/All.java
@@ -6,15 +6,15 @@ class All {
 
 	public static void main(String[] args) {
 		try {
-//			screenMorningstar(tickersIbkrCFDs("AU", 3), "xasx");
-//			screenMorningstar(tickersIbkrCFDs("BE", 1), "xbru");
-//			screenMorningstar(tickersIbkrCFDs("BR", 1), "bvmf");
-//			screenMorningstar(tickersIbkrCFDs("CZ", 1), "xpra");
-//			screenMorningstar(tickersIbkrCFDs("FI", 1), "xhel");
-//			screenMorningstar(tickersIbkrCFDs("FR", 2), "xpar");
-//			screenMorningstar(tickersIbkrCFDs("DE", 2), "xetr");
-//			screenMorningstar(tickersIbkrCFDs("HK", 2), "xhkg");
-//			screenMorningstar(tickersIbkrCFDs("JP", 11), "xtks");
+			screenMorningstar(tickersIbkrCFDs("AU", 3), "xasx");
+			screenMorningstar(tickersIbkrCFDs("BE", 1), "xbru");
+			screenMorningstar(tickersIbkrCFDs("BR", 1), "bvmf");
+			screenMorningstar(tickersIbkrCFDs("CZ", 1), "xpra");
+			screenMorningstar(tickersIbkrCFDs("FI", 1), "xhel");
+			screenMorningstar(tickersIbkrCFDs("FR", 2), "xpar");
+			screenMorningstar(tickersIbkrCFDs("DE", 2), "xetr");
+			screenMorningstar(tickersIbkrCFDs("HK", 2), "xhkg");
+			screenMorningstar(tickersIbkrCFDs("JP", 11), "xtks");
 			screenMorningstar(tickersIbkrCFDs("NL", 1), "xams");
 			screenMorningstar(tickersIbkrCFDs("NO", 2), "xosl");
 			screenMorningstar(tickersIbkrCFDs("PT", 2), "xlis");
@@ -24,8 +24,8 @@ class All {
 			screenMorningstar(tickersIbkrCFDs("SE", 2), "xsto");
 			screenMorningstar(tickersIbkrCFDs("CH", 2), "xswx");
 			screenMorningstar(tickersIbkrCFDs("GB", 6), "xlon");
-//			screenMorningstar(tickersIbkrCFDs("US", 36), "xnas");
-//			screenMorningstar(tickersIbkrCFDs("US", 36), "xnys");
+			screenMorningstar(tickersIbkrCFDs("US", 36), "xnas");
+			screenMorningstar(tickersIbkrCFDs("US", 36), "xnys");
 		} finally {
 			close();
 		}
